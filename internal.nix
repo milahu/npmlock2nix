13f8e94a27d6f509ea04e9b3887c71c472b8702f
Milan Hauth <milahu@gmail.com>
2024-03-24 20:07:35 +0100

    node_modules: allow missing src

--- internal.nix
+++ internal.nix
@@ -864,7 +864,7 @@ rec {
       throw "sourceHashFunc: spec.type '${spec.type}' is not supported. Supported types: 'github'";
 
   node_modules =
-    { src
+    { src ? /var/empty
     , packageJson ? src + "/package.json"
     , packageLockJson ? src + "/package-lock.json"
     , buildInputs ? [ ]
